fff00f5466dd8444bc8be2b066fbacc29b9a8679
Sumit Chaudhary <sumitchaudhary@live.com>
2020-08-19 15:56:22 -0700

    add pipenv

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
 import pycord.main as m
 
 m.hello()
+msg = m.reqip()
+print(msg)
--- pycord/main.py
+++ pycord/main.py
@@ -1,3 +1,9 @@
+import requests
+
+def reqip():
+    response = requests.get('https://httpbin.org/ip')
+    return('Your IP is {0}'.format(response.json()['origin']))
+
 def hello():
     print ('Hello World from function')
 
