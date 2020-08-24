774ed2cac09726e94c1925b1d877599cb8ad5aad
Sumit Chaudhary <sumitchaudhary@live.com>
2020-08-23 17:33:36 -0700

    hello bot working

--- pycord/bot.py
+++ pycord/bot.py
@@ -2,13 +2,26 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="..\.env")
+load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
+
+#print('Token is ___{0}____'.format(TOKEN))
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 @client.event
 async def on_message(message):
